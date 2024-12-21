bd29dd2ec2f76504dbf1e421447defe1311df8dc
ttinow <joao.panucci@hotmail.com>
2024-12-21 12:06:20 -0400

    commit final

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
 function calcularNivel() {
-    // Gerando valores aleatórios para vitórias e derrotas
-    const vitorias = Math.floor(Math.random() * 150);  // Vitórias entre 0 e 149
-    const derrotas = Math.floor(Math.random() * 100);   // Derrotas entre 0 e 99
+
+    const vitorias = Math.floor(Math.random() * 150);
+    const derrotas = Math.floor(Math.random() * 100);
     
-    // Calculando o saldo de vitórias
+
     const saldoVitorias = vitorias - derrotas;
-    
-    // Variável para armazenar o nível
+
     let nivel = '';
-  
-    // Estrutura de decisão para determinar o nível com base nas vitórias
-    if (vitorias < 10) {
-      nivel = 'Ferro';
-    } else if (vitorias >= 11 && vitorias <= 20) {
-      nivel = 'Bronze';
-    } else if (vitorias >= 21 && vitorias <= 50) {
-      nivel = 'Prata';
-    } else if (vitorias >= 51 && vitorias <= 80) {
-      nivel = 'Ouro';
-    } else if (vitorias >= 81 && vitorias <= 90) {
-      nivel = 'Diamante';
-    } else if (vitorias >= 91 && vitorias <= 100) {
-      nivel = 'Lendário';
-    } else if (vitorias >= 101) {
-      nivel = 'Imortal';
+
+    switch (true) {
+      case (vitorias < 10):
+        nivel = 'Ferro';
+        break;
+      case (vitorias >= 11 && vitorias <= 20):
+        nivel = 'Bronze';
+        break;
+      case (vitorias >= 21 && vitorias <= 50):
+        nivel = 'Prata';
+        break;
+      case (vitorias >= 51 && vitorias <= 80):
+        nivel = 'Ouro';
+        break;
+      case (vitorias >= 81 && vitorias <= 90):
+        nivel = 'Diamante';
+        break;
+      case (vitorias >= 91 && vitorias <= 100):
+        nivel = 'Lendário';
+        break;
+      case (vitorias >= 101):
+        nivel = 'Imortal';
+        break;
     }
   
-    // Exibindo a mensagem final com o saldo e o nível
     console.log(`Vitórias: ${vitorias}, Derrotas: ${derrotas}`);
     console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`);
   }
   
-  // Chamando a função para testar com valores aleatórios
+
   calcularNivel();
+
+
+  // **Execulte no terrminal do vscode, ele irá mostrar o numero de vitorias, derrotas e o ranking que você esta.
+  
   
